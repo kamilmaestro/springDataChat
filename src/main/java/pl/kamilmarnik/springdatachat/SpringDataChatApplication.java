bfb5522251a868eef233ec45bc32429477c38b79
Kamil Marnik <ksdekamil@gmail.com>
2019-06-18 21:38:42 +0200

    init project

---
+++ src/main/java/pl/kamilmarnik/springdatachat/SpringDataChatApplication.java
@@ -0,0 +1,13 @@
+package pl.kamilmarnik.springdatachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataChatApplication.class, args);
+    }
+
+}
